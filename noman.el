cf34b2e2238b8e89f8a33a057f9b81103608ebcf
andykuszyk <andy@kuszyk.com>
2023-12-24 19:52:23 +0000

    added initial menu function

--- noman.el
+++ noman.el
@@ -6,6 +6,12 @@
 
 (setq noman--last-command "")
 
+(setq noman--buttons '())
+
+(defun noman-menu (label)
+  (interactive (list (completing-read "prompt: " (cl-mapcar #'button-label noman--buttons))))
+  (message label))
+
 (defun noman--follow-link (button)
   "Follow a link from BUTTON in a noman buffer."
   (let ((subcommand (button-label button)))
@@ -15,6 +21,7 @@
   "Attempt to parse comand line help for the command CMD."
   (interactive "Mcommand: ")
   (setq noman--last-command cmd)
+  (setq noman--buttons '())
   (let ((buffer (get-buffer-create "noman")))
     (with-current-buffer buffer
       (read-only-mode -1)
@@ -34,7 +41,7 @@
 	      (let ((first-match (string-match "^ +\\([A-Za-z0-9\\-]+\\) \\{2\\}.*$" current-line-string)))
 		(when first-match
 		  (let ((beg (match-beginning 1)) (end (match-end 1)))
-		    (make-button (+ (line-beginning-position) beg) (+ (line-beginning-position) end) 'action #'noman--follow-link))))))
+		    (setq noman--buttons (append noman--buttons (list (make-button (+ (line-beginning-position) beg) (+ (line-beginning-position) end) 'action #'noman--follow-link)))))))))
 	  (forward-line)))
       (goto-char (point-min))
       (display-buffer buffer))))
