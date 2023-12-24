a6bb779387543e32fc1180a3e1bdbb64c8aa6f3a
andykuszyk <pairofsocks@hotmail.co.uk>
2023-12-24 12:05:45 +0000

    First working version

--- noman.el
+++ noman.el
@@ -4,17 +4,40 @@
 ;;; Code:
 (setq display-buffer-alist '(("noman" display-buffer-same-window)))
 
+(setq noman--last-command "")
+
+(defun noman--follow-link (button)
+  "Follow a link from BUTTON in a noman buffer."
+  (let ((subcommand (button-label button)))
+    (noman (format "%s %s" noman--last-command subcommand))))
+
 (defun noman (cmd)
   "Attempt to parse comand line help for the command CMD."
   (interactive "Mcommand: ")
+  (setq noman--last-command cmd)
   (let ((buffer (get-buffer-create "noman")))
     (with-current-buffer buffer
       (read-only-mode -1)
-      (erase-buffer buffer)
-      (call-process cmd nil buffer nil "--help")
+      (erase-buffer)
+      (let ((cmd-and-options (split-string cmd " ")))
+	(apply 'call-process
+	       (append
+		(list (nth 0 cmd-and-options) nil buffer nil)
+		(nthcdr 1 cmd-and-options)
+		(list "--help"))))
       (read-only-mode t)
-      (goto-char (point-min)))
-    (display-buffer buffer)))
+      (goto-char (point-min))
+      (let ((max-lines (count-lines (point-min) (point-max))))
+	(while (< (line-number-at-pos (point)) (+ max-lines 1))
+	  (let ((current-line-string (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
+	    (when (string-prefix-p "  " current-line-string)
+	      (let ((first-match (string-match "^ +\\([A-Za-z0-9\\-]+\\) \\{2\\}.*$" current-line-string)))
+		(when first-match
+		  (let ((beg (match-beginning 1)) (end (match-end 1)))
+		    (make-button (+ (line-beginning-position) beg) (+ (line-beginning-position) end) 'action #'noman--follow-link))))))
+	  (forward-line)))
+      (goto-char (point-min))
+      (display-buffer buffer))))
 
 (provide 'noman)
 ;;; noman.el ends here
